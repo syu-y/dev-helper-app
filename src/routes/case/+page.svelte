a2599dd8dc5df40f61f54e81f0484c8c7a4f022c
syu-y <syu@12580526@gmail.com>
2024-04-25 09:28:24 +0900

    ケース画面のレイアウト修正

--- src/routes/case/+page.svelte
+++ src/routes/case/+page.svelte
@@ -1,122 +1,121 @@
 <script lang="ts">
-  import PageSubTitle from '@/components/PageSubTitle.svelte';
-  import PageTitle from '@/components/PageTitle.svelte';
-  import TextInput from '@/components/form/TextInput.svelte';
-  import Label from '@components/form/Label.svelte';
-  import { strToCamelCase, strToSnakeCase } from '@utils/caseConvert';
+	import Link from '@/components/Link.svelte';
+	import PageSubTitle from '@/components/PageSubTitle.svelte';
+	import PageTitle from '@/components/PageTitle.svelte';
+	import TextInput from '@/components/form/TextInput.svelte';
+	import Label from '@components/form/Label.svelte';
+	import { strToCamelCase, strToSnakeCase } from '@utils/caseConvert';
 
-  let inCamelCase: string = '';
-  let outCamelCase: string = '';
+	let inCamelCase: string = '';
+	let outCamelCase: string = '';
 
-  const toCamelCase = (event: { currentTarget: HTMLInputElement }) => {
-    outCamelCase = strToCamelCase(event.currentTarget.value);
-  };
+	const toCamelCase = (event: { currentTarget: HTMLInputElement }) => {
+		outCamelCase = strToCamelCase(event.currentTarget.value);
+	};
 
-  let inSnakeCase: string = '';
-  let outSnakeCase: string = '';
-  const toSnakeCase = (event: { currentTarget: HTMLInputElement }) => {
-    outSnakeCase = strToSnakeCase(event.currentTarget.value);
-  };
+	let inSnakeCase: string = '';
+	let outSnakeCase: string = '';
+	const toSnakeCase = (event: { currentTarget: HTMLInputElement }) => {
+		outSnakeCase = strToSnakeCase(event.currentTarget.value);
+	};
 
-  // TODO: ケバブケース
-  // TODO: パスカルケースへの変換
-  // TODO: CSVファイルでまとめて変換
+	// TODO: ケバブケース
+	// TODO: パスカルケースへの変換
+	// TODO: CSVファイルでまとめて変換
 </script>
 
 <PageTitle>Case Convert</PageTitle>
 <div class="py-1">
-  <PageSubTitle>Convert Target</PageSubTitle>
-  <ul class="px-3 list-disc list-inside text-xm">
-    <li>camelCase</li>
-    <li>PascalCase</li>
-    <li>snake_case</li>
-    <li>kebab-case</li>
-  </ul>
+	<PageSubTitle>Convert Target</PageSubTitle>
+	<ul class="px-3 list-disc list-inside text-xm">
+		<li>camelCase</li>
+		<li>PascalCase</li>
+		<li>snake_case</li>
+		<li>kebab-case</li>
+	</ul>
 </div>
 
 <div class="py-1">
-  <h2>Convert Tool</h2>
-  <div class="p-2">
-    <h3>Convert to Camel Case</h3>
-    <div class="px-5">
-      <p>
-        <Label
-          labeltext="From(PascalCase/snake-case/kebab-case)"
-          name="inCamelCase"
-        />
-      </p>
-      <p class="px-5 py-1">
-        <TextInput
-          type="text"
-          id="inCamelCase"
-          name="inCamelCase"
-          change={toCamelCase}
-          bind:value={inCamelCase}
-        />
-      </p>
+	<h2>Convert Tool</h2>
+	<div class="p-2">
+		<h3>Convert to Camel Case</h3>
+		<div class="px-5">
+			<p>
+				<Label labeltext="From(PascalCase/snake-case/kebab-case)" name="inCamelCase" />
+			</p>
+			<p class="px-5 py-1">
+				<TextInput
+					type="text"
+					id="inCamelCase"
+					name="inCamelCase"
+					change={toCamelCase}
+					width="w-56"
+					bind:value={inCamelCase}
+				/>
+			</p>
 
-      <p>
-        <Label labeltext="To(CamelCase)" name="outCamelCase" />
-      </p>
-      <p class="px-5 py-1">
-        <TextInput
-          type="text"
-          id="outCamelCase"
-          name="outCamelCase"
-          readonly={true}
-          bind:value={outCamelCase}
-        />
-      </p>
-    </div>
-  </div>
+			<p>
+				<Label labeltext="To(CamelCase)" name="outCamelCase" />
+			</p>
+			<p class="px-5 py-1">
+				<TextInput
+					type="text"
+					id="outCamelCase"
+					name="outCamelCase"
+					readonly={true}
+					width="w-56"
+					bind:value={outCamelCase}
+				/>
+			</p>
+		</div>
+	</div>
 
-  <div class="p-2">
-    <h3>Convert to Snake Case</h3>
-    <div class="px-5">
-      <p>
-        <Label
-          labeltext="From(PascalCase/camelCase/kebab-case)"
-          name="inSnakeCase"
-        ></Label>
-      </p>
-      <p class="px-5 py-1">
-        <TextInput
-          type="text"
-          id="inSnakeCase"
-          name="inSnakeCase"
-          change={toSnakeCase}
-          bind:value={inSnakeCase}
-        />
-      </p>
+	<div class="p-2">
+		<h3>Convert to Snake Case</h3>
+		<div class="px-5">
+			<p>
+				<Label labeltext="From(PascalCase/camelCase/kebab-case)" name="inSnakeCase"></Label>
+			</p>
+			<p class="px-5 py-1">
+				<TextInput
+					type="text"
+					id="inSnakeCase"
+					name="inSnakeCase"
+					change={toSnakeCase}
+					width="w-56"
+					bind:value={inSnakeCase}
+				/>
+			</p>
 
-      <p>
-        <Label labeltext="To(SnakeCase)" name="outSnakeCase" />
-      </p>
-      <p class="px-5 py-1">
-        <TextInput
-          type="text"
-          id="outSnakeCase"
-          name="outSnakeCase"
-          readonly={true}
-          bind:value={outSnakeCase}
-        />
-      </p>
-    </div>
-  </div>
+			<p>
+				<Label labeltext="To(SnakeCase)" name="outSnakeCase" />
+			</p>
+			<p class="px-5 py-1">
+				<TextInput
+					type="text"
+					id="outSnakeCase"
+					name="outSnakeCase"
+					readonly={true}
+					width="w-56"
+					bind:value={outSnakeCase}
+				/>
+			</p>
+		</div>
+	</div>
 </div>
 
 <div></div>
 
 <div class="py-1">
-  <h2>Reference</h2>
-  <ul class="px-3 list-disc list-inside text-xm">
-    <li>
-      <a
-        href="https://typescript-jp.gitbook.io/deep-dive/styleguide"
-        target="_blank">TypeScriptスタイルガイド</a
-      >
-    </li>
-  </ul>
+	<h2>Reference</h2>
+	<ul class="px-3 list-disc list-inside text-xm">
+		<li>
+			<Link
+				url="https://typescript-jp.gitbook.io/deep-dive/styleguide"
+				label="TypeScriptスタイルガイド"
+			/>
+		</li>
+	</ul>
 </div>
 
 <style></style>
